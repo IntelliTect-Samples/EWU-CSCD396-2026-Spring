e40fc42cf12124f2a3f702d9bb2021cdb246072b
crawfordkid2 <114096196+crawfordkid2@users.noreply.github.com>
2026-04-07 15:46:12 -0700

    Implement temporary storage account creation and deletion
    Added creation and deletion of a temporary storage account.

--- Assignment1/Assignment1.ps1
+++ Assignment1/Assignment1.ps1
@@ -1,17 +1,29 @@
 # Assignment1.ps1
 
 $subscription_id = "f884461c-434c-4223-84b3-bc90b2906100"
+$resourceGroupName = "Assignment1-RG"
+$location = "eastus"
 
 Set-AzContext -SubscriptionId $subscription_id
+
+$randomSuffix = Get-Random -Maximum 99999
+$storageAccountName = "assign1temp$randomSuffix"
+
+Write-Host "Creating temporary storage account: $storageAccountName"
+
+New-AzStorageAccount `
+    -ResourceGroupName $resourceGroupName `
+    -Name $storageAccountName `
+    -Location $location `
+    -SkuName Standard_LRS `
+    -Kind StorageV2
 
 Write-Host "Listing resources in subscription: $subscription_id"
 Get-AzResource | Select-Object Name, ResourceGroupName, ResourceType, Location
 
-<#
-Application (client) ID
-399c2d72-4dbb-4b79-842c-ac5b6bc9bb94
-Object ID
-43d22a25-836a-4ac0-b356-9a6a12239507
-Directory (tenant) ID
-cbb8585a-58be-4c67-a9e8-aa46ea967bb1
-#>
+Write-Host "Deleting temporary storage account: $storageAccountName"
+
+Remove-AzStorageAccount `
+    -ResourceGroupName $resourceGroupName `
+    -Name $storageAccountName `
+    -Force
