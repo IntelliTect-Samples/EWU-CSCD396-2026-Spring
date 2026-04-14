457c42c0684ca6e6c2e64732626cf60ef800f911
wiilke <wkern1@ewu.edu>
2026-04-13 22:01:43 -0700

    Include powershell support

--- Assignment1/Assignment1.ps1
+++ Assignment1/Assignment1.ps1
@@ -1,6 +1,5 @@
 # Assignment 1 - Powershell Script for Azure Subscription
 $subscription_id = "207d6c46-9d83-44fc-b7d5-6e2cfcf4d001"
 
-Connect-AzAccount
 Set-AzContext -SubscriptionId $subscription_id
 Get-AzResource
