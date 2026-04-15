c815732f7a320b60985517636fed780b73fd8311
wiilke <wkern1@ewu.edu>
2026-04-14 23:43:08 -0700

    Final draft

--- Assignment1/Assignment1.ps1
+++ Assignment1/Assignment1.ps1
@@ -2,27 +2,19 @@
 $subscription_id = "207d6c46-9d83-44fc-b7d5-6e2cfcf4d001"
 Set-AzContext -SubscriptionId $subscription_id
 
-$location = "eastus"
+$resourceGroupName = "extracreditrgwiilke"
+$location = "centralus"
 
-# Check whether to create resource group
-$resourceGroupName = "extracreditrg"
-if (-not (Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue)) {
-    New-AzResourceGroup -Name $resourceGroupName -Location $location
-}
-
-# Same for storage account
-$storageAccountName = "extracreditsawiilke"
-if (-not (Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -ErrorAction SilentlyContinue)) {
-    New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -Location $location -SkuName Standard_LRS -Kind StorageV2
-}
+# Create resource group
+New-AzResourceGroup -Name $resourceGroupName -Location $location
 
 # Listing resources
-Write-Host "Resources:"
-Get-AzResource -ResourceGroupName $resourceGroupName
+Write-Host "Resource groups:"
+Get-AzResourceGroup
 
-# Delete storage account
-Remove-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -Force
+# Delete resource group
+Remove-AzResourceGroup -Name $resourceGroupName -Force
 
 # Listing resources again
-Write-Host "Resources after deletion:"
-Get-AzResource -ResourceGroupName $resourceGroupName
+Write-Host "Resource groups after deletion:"
+Get-AzResourceGroup
