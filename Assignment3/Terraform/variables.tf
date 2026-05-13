7059c9174ae2168f4c104d5daabb7c80a7eabb8a
William Kern <wkern1@ewu.edu>
2026-05-12 18:30:36 -0700

    files moved

---
+++ Assignment3/Terraform/variables.tf
@@ -0,0 +1,48 @@
+# Variables
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "assignment2-rg"
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "centralus"
+}
+
+variable "environment_name" {
+  description = "Name of the Container Apps Environment"
+  type        = string
+  default     = "assignment2"
+}
+
+variable "container_app_name" {
+  description = "Name of the Container App"
+  type        = string
+  default     = "assignment2ca"
+}
+
+variable "image_name" {
+  description = "Container image to deploy"
+  type        = string
+  default     = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
+}
+
+variable "container_registry_name" {
+  description = "Name of the Azure Container Registry"
+  type        = string
+  default     = "assignment2acrwiilke"
+}
+
+variable "service_bus_namespace_name" {
+  description = "Name of the Service Bus namespace"
+  type        = string
+  default     = "assignment3-sb-wiilke"
+}
+
+variable "service_bus_queue_name" {
+  description = "Name of the Service Bus queue"
+  type        = string
+  default     = "messages"
+}
